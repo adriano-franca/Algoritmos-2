da1b65b57787753fb3746bdd08ca6a20eaf55a0d
Adriano França Monteiro <adriano.franca@discente.ufma.br>
2025-05-16 11:01:51 -0300

    adicionando questão 401 resolvida em sala

---
+++ BinaryWatch401.py
@@ -0,0 +1,19 @@
+def time_to_string(hour, minute):
+        return str(hour)+":"+("0"+str(minute))[-2:]
+
+def leds(hour, minute):
+        return bin(hour).count("1") + bin(minute).count("1")
+
+def readBinaryWatch(turnedOn):
+    out = []
+    for i in range(12):
+        for j in range(60):
+            numLeds = leds(i, j)
+            if numLeds == turnedOn:
+                result = time_to_string(i, j)
+                out.append(result)
+    return out
+
+#Caso de teste
+turnedOn = 1
+print(readBinaryWatch(turnedOn))
